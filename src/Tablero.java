d45309e071a8d5354b03de7695b0170ced49c350
José Antonio Ortega Ruiz <joorter@unirioja.es>
2023-11-14 17:46:12 +0100

    gana booleano

--- src/Tablero.java
+++ src/Tablero.java
@@ -15,45 +15,181 @@ public class Tablero {
 			j = 0;
 		}
 	}
+
 	public void ponerFichaRoja(int columna) {
-		if(tablero[5][columna]==0) {
-			tablero[5][columna]=1;
-		}else {
-			int cont=5;
-			while((tablero[cont][columna]!=0)&&(cont>0)) {
+		if (tablero[5][columna] == 0) {
+			tablero[5][columna] = 1;
+		} else {
+			int cont = 5;
+			while ((tablero[cont][columna] != 0) && (cont > 0)) {
 				cont--;
 			}
-			tablero[cont][columna]=1;
-		}
-	}
-	
-	public void ponerFichaAmarilla(int columna) {
-		if(tablero[5][columna]==0) {
-			tablero[5][columna]=2;
-		}else {
-			int cont=5;
-			while((tablero[cont][columna]!=0) &&(cont>0)) {
-				cont--;
-			}
-			tablero[cont][columna]=2;
+			tablero[cont][columna] = 1;
 		}
 	}
 
+	public void ponerFichaAmarilla(int columna) {
+		if (tablero[5][columna] == 0) {
+			tablero[5][columna] = 2;
+		} else {
+			int cont = 5;
+			while ((tablero[cont][columna] != 0) && (cont > 0)) {
+				cont--;
+			}
+			tablero[cont][columna] = 2;
+		}
+	}
+	public boolean ganarojo(int i,int j) {
+		int cont=0;
+		int suma=0;
+		while(cont<7) {
+			if(tablero[cont][j]==1) {
+				suma++;
+				if(suma==4) {
+					return true;
+				}
+			}else {
+				suma=0;
+			}
+			cont++;
+		}
+		 cont=0;
+		 suma=0;
+		while(cont<7) {
+			if(tablero[i][cont]==1) {
+				suma++;
+				if(suma==4) {
+					return true;
+				}
+			}else {
+				suma=0;
+			}
+			cont++;
+		}
+		 cont=0;
+		 suma=0;
+		int iaux=i;
+		int jaux=j;
+		while(iaux>0 && jaux>0) {
+			iaux--;
+			jaux--;
+		}
+		while(iaux<6 && jaux<7) {
+			if(tablero[iaux][jaux]==1) {
+				suma++;
+				if(suma==4) {
+					return true;
+				}
+			}else {
+				suma=0;
+			}
+			iaux++;
+			jaux++;
+		}
+		 iaux=i;
+		 jaux=j;
+		while(iaux>0 && jaux<7) {
+			iaux--;
+			jaux++;
+		}
+		while(iaux>0 && jaux<7) {
+			if(tablero[iaux][jaux]==1) {
+				suma++;
+				if(suma==4) {
+					return true;
+				}
+			}else {
+				suma=0;
+			}
+			iaux++;
+			jaux--;
+		}
+		return false;
+				
+	}
+	public boolean ganaamarillo(int i,int j) {
+		int cont=0;
+		int suma=0;
+		while(cont<7) {
+			if(tablero[cont][j]==2) {
+				suma++;
+				if(suma==4) {
+					return true;
+				}
+			}else {
+				suma=0;
+			}
+			cont++;
+		}
+		 cont=0;
+		 suma=0;
+		while(cont<7) {
+			if(tablero[i][cont]==2) {
+				suma++;
+				if(suma==4) {
+					return true;
+				}
+			}else {
+				suma=0;
+			}
+			cont++;
+		}
+		 cont=0;
+		 suma=0;
+		int iaux=i;
+		int jaux=j;
+		while(iaux>0 && jaux>0) {
+			iaux--;
+			jaux--;
+		}
+		while(iaux<6 && jaux<7) {
+			if(tablero[iaux][jaux]==2) {
+				suma++;
+				if(suma==4) {
+					return true;
+				}
+			}else {
+				suma=0;
+			}
+			iaux++;
+			jaux++;
+		}
+		 iaux=i;
+		 jaux=j;
+		while(iaux>0 && jaux<7) {
+			iaux--;
+			jaux++;
+		}
+		while(iaux>0 && jaux<7) {
+			if(tablero[iaux][jaux]==2) {
+				suma++;
+				if(suma==4) {
+					return true;
+				}
+			}else {
+				suma=0;
+			}
+			iaux++;
+			jaux--;
+		}
+		return false;
+				
+	}
+	
 	public void mostrarTablero() {
 		for (int x = 0; x < tablero.length; x++) {
 			for (int y = 0; y < tablero[x].length; y++) {
 				System.out.print("|");
-				if(tablero[x][y]==0) {
+				if (tablero[x][y] == 0) {
 					System.out.print(" ");
 				}
-				if(tablero[x][y]==1) {
+				if (tablero[x][y] == 1) {
 					System.out.print("X");
 				}
-				if(tablero[x][y]==2) {
+				if (tablero[x][y] == 2) {
 					System.out.print("O");
 				}
-				
-				
+
 			}
 			System.out.println("|");
 		}
