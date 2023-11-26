ad6a3184c126975306faa55856fc3b5d6f6decbf
15mar <15mar@192.168.1.107>
2023-11-26 14:58:09 +0100

    logros

---
+++ src/ActualizarLogros.java
@@ -0,0 +1,13 @@
+import java.io.File;
+
+public class ActualizarLogros {
+	public static void main(String[]args) {
+		try {
+			File xmlFile = new File("BD.xml");
+		}
+		catch (Exception e) {
+            e.printStackTrace();
+        }
+	}
+
+}
